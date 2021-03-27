63a6f8b5f1339eb78e071590025f498fc7d38e55
Parvinderjit Singh <parv112281@hotmail.com>
2021-03-27 10:37:14 -0400

    add some test code for extracting data and some raw data

---
+++ protein-sequences/naive_bayes.py
@@ -0,0 +1,23 @@
+import json
+import re
+from nltk.tokenize import sent_tokenize, word_tokenize
+import spacy
+
+data = None
+raw_sentences = {}
+sentences = {}
+
+with open("/Users/parvinderjit.singh/Desktop/Harvard/CSCI E-599/Patent_Visualization_Project/text-mining-poc/protein-sequences/test_data.json", "r") as read_file:
+    data = json.load(read_file)
+for patent in data:
+    raw_sentences[patent["patentid"]] = []
+    raw_sentences[patent["patentid"]] += sent_tokenize(patent['abstract'])
+    raw_sentences[patent["patentid"]] += sent_tokenize(patent['claims'])
+    raw_sentences[patent["patentid"]] += sent_tokenize(patent['description'])
+for k in raw_sentences.keys():
+    sentences[k] = []
+    for s in raw_sentences[k]:
+        if re.search('SEQ ID', s, re.IGNORECASE):
+            sentences[k].append(s)
+for k in sentences.keys():
+    print(k, len(sentences[k]))
